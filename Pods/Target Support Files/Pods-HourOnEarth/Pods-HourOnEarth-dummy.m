161d4d893e0223517f7d0b7d7717e8a8a0bcbab5
deepakjain1231 <deepakjain1231@gmail.com>
2025-09-26 13:07:20 +0530

    Add pod file

---
+++ Pods/Target Support Files/Pods-HourOnEarth/Pods-HourOnEarth-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HourOnEarth : NSObject
+@end
+@implementation PodsDummy_Pods_HourOnEarth
+@end
---
+++ Pods/Target Support Files/ViewDeck/ViewDeck-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "IISideController.h"
+#import "IIViewDeckController.h"
+#import "IIWrapController.h"
+#import "ViewDeck.h"
+
+FOUNDATION_EXPORT double ViewDeckVersionNumber;
+FOUNDATION_EXPORT const unsigned char ViewDeckVersionString[];
+
